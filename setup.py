ccc291e79b07c4b2d8b0819bfadd071ab24482ea
1u1s4 <3553751921101@ingenieria.usac.edu.gt>
2023-07-22 21:55:25 -0600

    Initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+setup(
+    name='INEfnts',
+    version='0.1',
+    author='Luis Alfredo Alvarado Rodríguez',
+    description='ETL para las fuentes del IPC/CBA.',
+    long_description='',
+    url='https://github.com/1u1s4/INEfnts',
+    keywords='development, setup, setuptools',
+    python_requires='>=3.7',
+    packages=find_packages(),
+    py_modules=['Fuentes'],
+    install_requires=[
+        'pyodbc',
+        'pandas',
+    ]
+)
