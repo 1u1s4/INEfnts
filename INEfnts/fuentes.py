feca755aac711382bd64495fa773e5d545023251
Luis Alvarado <99617682+1u1s4@users.noreply.github.com>
2023-08-03 20:53:04 -0600

    Update fuentes.py

--- INEfnts/fuentes.py
+++ INEfnts/fuentes.py
@@ -37,7 +37,7 @@ class Fuentes:
 
         # Genera una lista de los últimos 12 meses
         start_date = datetime.date(anio, mes, 1)
-        dates = [start_date - relativedelta(months=i) for i in range(12)]
+        dates = [start_date - relativedelta(months=i) for i in range(13)]
         
         # Llama a la función boletas para cada mes y guarda los resultados en dfs
         for date in dates:
